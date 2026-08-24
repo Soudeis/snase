8cc0a6907a6212092b7f0dbe0a543c7f9701b148
Abakoura <abakouraamir@gmail.com>
2026-08-25 01:13:08 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // Adresse de ton API Go. Si le frontend est servi par le même serveur Go
 // (via /app/ comme indiqué dans README_INTEGRATION.md), laisser une chaîne vide.
-const API_BASE = "http://api.snase.org:8080";
+const API_BASE = "https://api.snase.org";
